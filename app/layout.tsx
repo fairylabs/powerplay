8f012f6147195c4d52533a166271fb7b29127feb
maggo <me@mar.co.de>
2024-02-13 00:07:12 +0100

    add opengraph stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,10 @@ import "./globals.css";
 import { config } from "./wagmi";
 
 export const metadata: Metadata = {
-  title: "Powerbald",
-  description: "The most $DEGEN lottery on Farcaster!",
+  title: "Powerbald - The most $DEGEN lottery on farcaster",
+  description: "Your chance to win 1,000,000 $DEGEN every day",
+  icons:
+    "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔵</text></svg>",
 };
 
 export default function RootLayout({
